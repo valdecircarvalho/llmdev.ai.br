41ddb099d3d479195ed0b919488df90dd8f59abd
valdecircarvalho <valdecir.carvalho@outlook.com>
2026-02-14 16:49:06 -0300

    feat: apply saral theme

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,26 @@
-import { getCollection } from 'astro:content';
-import rss from '@astrojs/rss';
-import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
+import rss from '@astrojs/rss'
+import { getCollection } from 'astro:content'
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
+	const blog = await getCollection('blog')
 	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
+		// `<title>` field in output xml
+		title: 'Saral Theme',
+		// `<description>` field in output xml
+		description:
+			'A simple theme for personal blog sites, created for Astro framework',
+		// Pull in your project "site" from the endpoint context
+		// https://docs.astro.build/en/reference/api-reference/#site
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
+		// Array of `<item>`s in output xml
+		// See "Generating items" section for examples using content collections and glob imports
+		items: blog.map((post) => ({
+			title: post.data.title,
+			pubDate: post.data.pubDate,
+			description: post.data.description,
+			// Compute RSS link from post `id`
+			// This example assumes all posts are rendered as `/blog/[id]` routes
+			link: `/blog/${post.id.replace('.md', '')}/`,
 		})),
-	});
+	})
 }
